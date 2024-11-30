1abb1a23e7913b4c6e4d08456d8e3830f7d4f4e9
Akash Daud <142325556+Akash-Daud@users.noreply.github.com>
2024-11-30 09:17:28 +0000

    02days commit

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,33 @@
+ const accountID = 12345
+ let accountEmail ="akash@gmail.com"
+ var accountPassword = "1234"
+ accountCity = "pune"
+
+  //accountID = 54321   not allowed becouse this is the allredy decleare as a const 
+  accountEmail = "daud@gmail.com"
+
+  accountPassword = 4321
+
+//  console.log(accountID);
+//  console.log(accountEmail);
+//  console.log(accountPassword);
+//  console.log(accountCity);
+
+console.table([accountID ,accountEmail ])  //This the console table 
+
+ /*Notes
+
+var = var is the functional scope he is suppot the hosting & shadowing 
+    var is you can 
+       var;     inti
+     var= a;      assign 
+     a=10;       declering
+
+let = is the block scope introduce the ES6 javascrpit and aslo dont support  hosting & shadowing 
+  let;     inti
+let= a;      assign 
+a=10;       declering
+
+const=is the block scope introduce the ES6 javascrpit and aslo dont support  hosting & shadowing 
+
+var a=10;  only decl */
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,20 @@
+ //Datatype  in javascrpit ECMA standards
+
+ "use strict" //treat all JS code newer version 
+
+   let = "Akash"  // string 
+   let = 23        //number 
+   let logout = true  // boolean
+   
+//    Data type 
+//    string =""
+//    integer  = 2 to the power of 53 
+//    BigInt   = gig number 
+//     Boolean  = true/false
+//     null     = standalone value   
+//     undefined
+//     symbol =unique
+
+console.log(typeof undefined);  //undefined
+
+console.log (typeof null);   //object
