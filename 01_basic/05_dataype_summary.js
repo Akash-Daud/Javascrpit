98abd21506f2bc5831ff0b0eeaa0df6c2f66ad16
Akash Daud <142325556+Akash-Daud@users.noreply.github.com>
2024-11-30 17:48:35 +0000

    day 5

---
+++ 01_basic/05_dataype_summary.js
@@ -0,0 +1,57 @@
+// Primative 
+  /*string 
+ Number
+ boolean 
+ null
+ undefined
+symbol 
+Bigint  */
+
+
+// Javascript Is Dynamicaly type langauge 
+  const score =100
+//   const scorwvalue=100.9
+
+//   const isLogIn = false
+//   const outsideTem=null
+//   let userEmail;    //undifine
+
+
+// const id= Symbol('12345')  it`s unique
+// const id2= Symbol('12345')
+
+//     id === id2    ---> it`s not same value
+
+
+
+
+// Non Primative (Refrence Type)
+/*Array 
+
+const heros ["akash ", "ravi","gopal"]
+
+
+Object
+
+let myObj={
+ name ="akash ",
+ age:23,
+}
+
+Function */
+normal function and arrow function
+
+const myFunction = function(){
+    console.log(" Normal function")
+}
+
+myFunction=()=>{
+    console.log("This is the arrow function");
+    
+}
+
+how to check a datatype 
+
+   --> typeOf 
+
+   note = all nonpremative datatype is show the typeof is Object
