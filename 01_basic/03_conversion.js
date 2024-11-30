556e453effb86b73637957f49e378101a2ba5010
Akash Daud <142325556+Akash-Daud@users.noreply.github.com>
2024-11-30 17:08:30 +0000

    Day 04 commit

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,80 @@
+// let score = "Akash"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueNumber = Number(score)
+// console.log(valueNumber);
+// console.log(typeof valueNumber);
+
+// "33"--- > 33
+// "33abc" --> NaN
+// true ---> 1; false--> 0;
+
+// Boolean value check for the testing 
+
+// let isLogin = 20;
+
+// let booleanIsLogin = Boolean(isLogin)
+
+// console.log(booleanIsLogin);
+// console.log(tyepof booleanIsLogin);
+
+// 1 => true   
+// 0 => false
+// "akash"=> true
+
+//string conversion
+
+
+// javascrpit is the case sensetive 
+
+// let stringNumber = 33;
+
+// let stringConvert = String(stringNumber)
+
+// // console.log(stringConvert);
+// console.log( typeof stringConvert);
+
+
+
+
+/************  Operations  **************/
+
+let value = 3
+
+let negValue  = -value
+
+// console.log(negValue);
+
+
+// console.log(2+2); addition 
+// console.log(2*2);      mul
+// console.log(2-2);     sub
+// console.log(2**2);  power
+// console.log(2%2);  modulo
+
+
+// let str1 = "Akash"
+// let str2 = " Daud"
+
+// let str3=  str1 + str2
+
+// console.log(str3);
+
+// atrhamatic operation 
+
+// console.log("1" +2);      //12
+// console.log(1 + "2");     //12
+// console.log( "1"+ 2+2);   //122
+// console.log(1+2 "2");    //32 output
+
+// documantion readding the ECMA scrpit
+
+let num1, num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter =100
+gameCounter--;
+console.log(gameCounter);
---
+++ 01_basic/04_Comparison.js
@@ -0,0 +1,22 @@
+// Comaparison of the datatype
+
+console.log("2" > 1);
+console.log("02">1);
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+
+console.log("2"==2);
+
+
+==  ---> only check the value 
+=== ---> check the datatype and value 
