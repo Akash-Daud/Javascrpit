26ae93fe9528cd6aa0e19327aa8977489d6b62dc
Akash Daud <142325556+Akash-Daud@users.noreply.github.com>
2024-12-02 07:58:58 +0000

    day 8

---
+++ 01_basic/08_date_Time.js
@@ -0,0 +1,26 @@
+// let myDate=new Date()
+// console.log(myDate.toString());
+
+// console.log(myDate.toDateString ());
+// console.log(myDate.toISOString ());
+// console.log(myDate.toJSON ());
+// console.log(myDate.toLocaleDateString ());
+// console.log(myDate.toLocaleTimeString ());
+
+let myCreateDate = new Date(2001 ,11 ,26)
+console.log( myCreateDate.toDateString());
+
+
+let myTimeStamp =Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreateDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+
+newDate.toDateString('default',
+
+    weekday:"long",
+    
+)
