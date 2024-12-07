ec4af43180f26bcb3e9cd303cb19c35a954a8e88
Akash Daud <142325556+Akash-Daud@users.noreply.github.com>
2024-12-07 16:39:39 +0000

    json

---
+++ 02_basic/05_object3.js
@@ -0,0 +1,32 @@
+const  course ={
+    coursename :"flipcard",
+    price:"999",
+    courseInstr:"hitesh"
+}
+
+
+// you want to specical value that timr you are yousing 
+
+course.courseInstr
+
+const {courseInstr}= course
+
+console.log(courseInstr);
+
+
+// API concept responce
+// 1){
+//     "name":"Akash",
+//     "couseID":"ID23",
+//      "price":"free"
+// }
+
+// 2)
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
+
+ 
